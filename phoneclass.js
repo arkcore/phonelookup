45d15507837089297ac379d25204b0f6d6e6a08d
afoninsky <vkfont@gmail.com>
2014-03-16 21:46:10 +0400

    all world codes support, get info from number

---
+++ phoneclass.js
@@ -0,0 +1,98 @@
+'use srtict';
+
+var libxmljs = require('libxmljs'),
+	fs = require('fs'),
+	_ = require('lodash');
+
+
+exports.PhoneClass = function() {
+
+	var phoneTypes = ['fixedLine', 'mobile', 'personalNumber']; // phones we are iterested in
+
+/*
+ * Load country settings
+ */
+ 	var shema = {};
+	(function() {
+		var xml = libxmljs.parseXml(fs.readFileSync('PhoneNumberMetadata.xml'));
+		xml.get('/phoneNumberMetadata/territories').childNodes().forEach(function(item) {
+			if(item.name() !== 'territory') { return; }
+			
+			var territory = {
+				id: 			item.attr('id').value(),
+				countryCode: 	item.attr('countryCode').value(),
+				patterns: 		{},
+				// nationalPattern: new RegExp(item.get('generalDesc/nationalNumberPattern').text().replace(/[\s\n]/gm,'')),
+				phoneLength: 	null
+			}
+			_(phoneTypes).each(function(val) {
+				var t = item.get(val+'/nationalNumberPattern');
+				var e = item.get(val+'/exampleNumber');
+				if(!t || !e) { return; }
+				if(!territory.phoneLength) { territory.phoneLength = e.text().length; }
+				territory.patterns[val] = new RegExp(t.text().replace(/[\s\n]/gm,''))
+			});
+			shema[territory.id] = territory;
+		});
+	})();
+
+/*
+ * Append numbers to code till full length
+ */
+	var padPhone = function(code, length, isMax) {
+		if(code.length > length) { throw new Error('phone length should be lower than '+length); }
+		return code + (new Array(length - code.length + 1).join(isMax?'9':'0'));
+	}
+
+
+/*
+ * Try to find country id by number (or its part). First guess returned
+ */
+	var countryFromNumber = function(num) {
+		var guess = [];
+		_(shema).each(function(item) {
+			if(item.countryCode === num.substring(0, item.countryCode.length)) {
+				guess.push({
+					id: item.id,
+					code: item.countryCode,
+					line: num.substring(item.countryCode.length)
+				});
+			}
+		});
+		if(!guess.length) { return; } // first pass - code not found
+
+		var ret;
+		_(guess).each(function(item) {
+			var ter = shema[item.id];
+			var phone = padPhone(item.line, ter.phoneLength);
+			_(ter.patterns).each(function(pat, key) {
+				if(pat.test(phone)) { // !!! found
+					return ret = {
+						country: 	item.id,
+						code: 		item.code,
+						line: 		item.line,
+						type: 		key
+					}
+				}
+			});
+		});
+		return ret;
+	}
+
+/*
+ * 
+ */
+	this.nextPhone = function() {
+
+	}
+
+
+/*
+ * 
+ */
+	this.isPhoneValid = function(phone) {
+
+	}
+
+
+}
