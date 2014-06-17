53f667a04161d5ccfd7ae5318bd359d378f4118b
vkfont <vkfont@gmail.com>
2014-06-17 16:32:45 +0400

    update features

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ phone-lookup
 ============
 
 phone range generation, per country validation and lookup for links to social profiles
+
+
+*Usage quick examples:*
+
+```
+var Phone = require('phone-lookup');
+
+var test = new Phone;
+
+// iterate over range finding all phones can exist
+var it = new phone.Iterator('78182')
+console.log(iterator.getNext())
+console.log(iterator.getNext())
+
+// test if phone valid (correct format for country, available in number plans for country)
+test.isPhoneValid('+14857384959335')
+
+// try to convert phone number to international format
+test.formatPhone('8-800-CALL NOW'); // ex.: +8-800-1212-323
+```
--- test.js
+++ test.js
@@ -1,9 +1,21 @@
 'use srtict';
 
-var Phone = require('./phoneclass').PhoneClass;
+var utils = require('./lib/utils');
 
-var phone = new Phone;
-var iterator = new phone.Iterator('78182')
-// var iterator2 = new phone.Iterator('11')
-console.log(iterator.getNext())
-console.log(iterator.getNext())
+var Phone = require('./');
+
+var test = new Phone;
+// console.log(range);
+
+// var phone = new Phone;
+// var iterator = new phone.Iterator('78182')
+// // var iterator2 = new phone.Iterator('11')
+// console.log(iterator.getNext())
+// console.log(iterator.getNext())
+var it = new test.Iterator('1');
+
+console.log(it.getNext());
+
+
+// isPhoneValid
+// formatPhone
