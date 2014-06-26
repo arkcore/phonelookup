96265a35f357e122a59beccbe6a348ccd6b989a0
afoninsky <vkfont@gmail.com>
2014-06-26 14:28:00 +0400

    bugfixes

--- test.js
+++ test.js
@@ -5,22 +5,10 @@ var utils = require('./lib/utils');
 var Phone = require('./');
 
 var test = new Phone();
-// console.log(range);
-
-// var phone = new Phone;
-// var iterator = new phone.Iterator('78182')
-// // var iterator2 = new phone.Iterator('11')
-// console.log(iterator.getNext())
-// console.log(iterator.getNext())
-// var it = new test.Iterator('1');
-
-// var phone = test.format('+7921-241906a:)');
-var before = '+1 212 CALL NOW';
-var after = test.normalize(before);
-
-console.log('before: %s, after: %s', before, after);
-
-console.log(test.isValid(after));
-
-// isPhoneValid
-// formatPhone
+// var cfn = test.countryFromNumber(18773975248);
+var ph = '+18.773975248';
+// var ph = '+78182-613101';
+console.log('result:', test.countryFromNumber(ph));
+// console.log('formatted =', test.format(ph));
+// process.exit();
+// console.log('valid =', test.isValid(ph));
